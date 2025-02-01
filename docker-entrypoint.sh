50433ab856a5e9c099a6ec88b115dc66aac13211
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2025-01-31 17:37:56 -0800

    Update docker-entrypoint.sh
    - create relevant directories if they don't exist already
    - copy over template files if applicable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,32 @@ EASY_RSA=/usr/share/easy-rsa
 OPENVPN_DIR=/etc/openvpn
 echo "EasyRSA path: $EASY_RSA OVPN path: $OPENVPN_DIR"
 
+CFG_TMPLT_DIR=/opt/app/template
+echo "Template files path: $CFG_TMPLT_DIR"
+
+# create initial directories in mapped volume if they don't exist
+if [ ! -d $OPENVPN_DIR/config ]; then
+    mkdir $OPENVPN_DIR/config
+    cp $CFG_TMPLT_DIR/* $OPENVPN_DIR/config
+    if [ ! -f $OPENVPN_DIR/server.conf ]; then
+       cp $OPENVPN_DIR/template/server.conf $OPENVPN_DIR/server.conf
+    fi
+fi
+if [ ! -d $OPENVPN_DIR/clients ]; then
+    mkdir $OPENVPN_DIR/clients
+fi
+if [ ! -d $OPENVPN_DIR/db ]; then
+    mkdir $OPENVPN_DIR/db
+fi
+if [ ! -d $OPENVPN_DIR/log ]; then
+    mkdir $OPENVPN_DIR/log
+fi
+if [ ! -d $OPENVPN_DIR/staticclients ]; then
+    mkdir $OPENVPN_DIR/staticclients
+fi
+if [ ! -d $OPENVPN_DIR/pki ]; then
+    mkdir $OPENVPN_DIR/pki
+fi
 if [[ ! -f $OPENVPN_DIR/pki/ca.crt ]]; then
     export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
     cd $EASY_RSA
