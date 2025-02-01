4190c5640a8009482ef2434e80a0d21bc500c611
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2025-01-31 17:48:37 -0800

    Update docker-entrypoint.sh
    move initial directory creation to separate file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,34 +7,8 @@ EASY_RSA=/usr/share/easy-rsa
 OPENVPN_DIR=/etc/openvpn
 echo "EasyRSA path: $EASY_RSA OVPN path: $OPENVPN_DIR"
 
-CFG_TMPLT_DIR=/opt/app/template
-echo "Template files path: $CFG_TMPLT_DIR"
-
-# create initial directories in mapped volume if they don't exist
-if [ ! -d $OPENVPN_DIR/config ]; then
-    mkdir $OPENVPN_DIR/config
-    cp $CFG_TMPLT_DIR/* $OPENVPN_DIR/config
-fi
-if [ ! -d $OPENVPN_DIR/clients ]; then
-    mkdir $OPENVPN_DIR/clients
-fi
-if [ ! -d $OPENVPN_DIR/db ]; then
-    mkdir $OPENVPN_DIR/db
-fi
-if [ ! -d $OPENVPN_DIR/log ]; then
-    mkdir $OPENVPN_DIR/log
-fi
-if [ ! -d $OPENVPN_DIR/staticclients ]; then
-    mkdir $OPENVPN_DIR/staticclients
-fi
-if [ ! -d $OPENVPN_DIR/pki ]; then
-    mkdir $OPENVPN_DIR/pki
-fi
-
-# if server.conf doesn't exist, pull from template directory as a starting point
-if [ ! -f $OPENVPN_DIR/server.conf ]; then
-   cp $OPENVPN_DIR/template/server.conf $OPENVPN_DIR/server.conf
-fi
+# invoke initial directory and file creation
+source dir_ini.sh
 
 if [[ ! -f $OPENVPN_DIR/pki/ca.crt ]]; then
     export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
